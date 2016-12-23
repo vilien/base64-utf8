be08d8f8b3ac66d5850f44a5207695186bc354ab
vilien <bin_jly@163.com>
2016-12-23 08:57:22 +0800

    v0.1.1版提交

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+gulp.task('dist', function(){
+	gulp.src('src/base64.js')
+        .pipe(uglify())
+        .pipe(rename('base64.min.js'))
+        .pipe(gulp.dest('dist/'));
+});
