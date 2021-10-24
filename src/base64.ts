4bd9a0f2e1d33ce01448999d12dd7bcd3894cdeb
vilien <bin_jly@163.com>
2021-10-24 21:25:51 +0800

    refactor: modify encode

--- src/base64.ts
+++ src/base64.ts
@@ -1,42 +1,39 @@
-const charMap = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+const CHAR_MAP = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
 
 const encode = (input: string): string => {
-  var str = String(input);
-  var map = charMap;
-  var block = 0,
-      output = '';
-  var prx = [2, 4, 6, 8];
-  for (var code, idx = 3 / 4, uarr;
-  // 能取到字符时、block未处理完时、长度不足时
-  !isNaN(code = str.charCodeAt(idx)) || 63 & block || (map = '=', (idx - 3 / 4) % 1); idx += 3 / 4) {
-    if (code > 0x7F) {
-      // utf8字符处理
-      (uarr = encodeURI(str.charAt(idx)).split('%')).shift();
-      for (var hex, idx2 = idx % 1; hex = uarr[idx2 | 0]; idx2 += 3 / 4) {
-        block = block << 8 | parseInt(hex, 16);
-        output += map.charAt(63 & block >> 8 - idx2 % 1 * 8);
+  const str = String(input);
+  const prx = [2, 4, 6, 8];
+  let map = CHAR_MAP;
+  let output = '';
+  let block = 0;
+  for (let i = 0, j = 0; i < str.length || 63 & block || (map = '=', j % 4 !== 0); i++) {
+    const code = str.charCodeAt(i);
+    const uarr: any[] = code > 0x7F ? encodeURI(str.charAt(i)).split('%').slice(1).map(c => parseInt(c, 16)) : [code];
+    for (let k = 0; k < uarr.length; k++) {
+      block = block << 8 | uarr[k];
+      output += map.charAt(63 & block >> prx[j % 4]);
+      j++;
+      if (j % 4 === 3) { // tail
+        output += isNaN(uarr[k]) ? '=' : map.charAt(63 & block);
+        j++;
+        block = 0;
       }
-      idx = idx === 3 / 4 ? 0 : idx; // 修复首字符为utf8字符时出错的BUG
-      idx += 3 / 4 * uarr.length % 1; // idx补偿
-    } else {
-      block = block << 8 | code;
-      output += map.charAt(63 & block >> 8 - idx % 1 * 8);
     }
   }
   return output;
 }
 
 const decode = (input: string): string => {
-  var str = String(input),
-      map = charMap.slice(0, -1),
-      prx = [6, 4, 2, 0],
-      output = '',
-      block = 0,
-      code,
-      buffer = 0,
-      hex = '';
+  const str = String(input);
+  const prx = [6, 4, 2, 0];
+  let map = CHAR_MAP.slice(0, -1);
+  let output = '';
+  let block = 0;
+  let code;
+  let buffer = 0;
+  let hex = '';
   try {
-    for (var i = 0; (code = map.indexOf(str[i])) > -1; i++) {
+    for (let i = 0; (code = map.indexOf(str[i])) > -1; i++) {
       block = block << 6 | code;
       if (i % 4) {
         buffer = 255 & block >> prx[i % 4];
@@ -52,11 +49,9 @@ const decode = (input: string): string => {
     output += hex ? decodeURI(hex) : '';
     return output;
   } catch (err) {
-    // console.log(err);
     throw new Error('base64 malformed!');
   }
 }
-
 
 export default {
   encode,
